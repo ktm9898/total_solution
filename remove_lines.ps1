f9ac27f8112b559763c6d1cf8c0b4c65c759b400
ktm9898 <ktm9898@github.com>
2026-02-22 13:19:44 +0900

    Cleanup: Remove temporary script files

--- remove_lines.ps1
+++
@@ -1,4 +0,0 @@
-$lines = [System.IO.File]::ReadAllLines("admin.html", [System.Text.Encoding]::UTF8)
-$newLines = $lines[0..901] + $lines[1034..($lines.Length - 1)]
-[System.IO.File]::WriteAllLines("admin.html", $newLines, [System.Text.Encoding]::UTF8)
-Write-Output "Done"
